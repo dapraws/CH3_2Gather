74d3c10001fd2f0d4bcb21deca70f7377660d22b
Ryan <ryanmfdr@gmail.com>
2026-06-02 14:19:35 +0800

    feat: init Design System

--- 2Gather/Themes/Colors.swift
+++ 2Gather/Themes/Colors.swift
@@ -4,3 +4,58 @@
 //
 //  Created by Muhammad Darrel Prawira on 25/05/26.
 //
+
+import SwiftUI
+
+// MARK: - Hex Helper
+
+extension Color {
+    init(hex: String) {
+        var s = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        if s.hasPrefix("#") { s.removeFirst() }
+        var value: UInt64 = 0
+        Scanner(string: s).scanHexInt64(&value)
+        self.init(
+            .sRGB,
+            red:     Double((value >> 16) & 0xFF) / 255,
+            green:   Double((value >>  8) & 0xFF) / 255,
+            blue:    Double( value        & 0xFF) / 255,
+            opacity: s.count == 8 ? Double(value & 0xFF) / 255 : 1.0
+        )
+    }
+}
+
+// MARK: - Design System Colors
+
+extension Color {
+
+    // MARK: Brand
+    static let primary   = Color(hex: "#4A60B2")   // Brown
+    static let secondary = Color(hex: "#FF7606")   // Orange
+    static let terniary    = Color(hex: "#FFDC49")   // Yellow
+
+    // MARK: Background
+    static let backgroundPrimary   = Color(.systemBackground)
+    static let backgroundSecondary = Color(.secondarySystemBackground)
+    static let backgroundElevated  = Color(.tertiarySystemBackground)
+
+    // MARK: Text
+    static let textPrimary   = Color(.label)
+    static let textSecondary = Color(.secondaryLabel)
+    static let textDisabled  = Color(.tertiaryLabel)
+
+    // MARK: Border
+    static let borderDefault = Color(.separator)
+
+    // MARK: Status
+    static let success = Color(.systemGreen)
+    static let warning = Color(.systemOrange)
+    static let error   = Color(.systemRed)
+    static let info    = Color(.systemBlue)
+
+    // MARK: Sport
+    static let scoreWin      = Color(.systemGreen)
+    static let scoreLoss     = Color(.systemRed)
+    static let scoreDraw     = Color(.systemGray)
+    static let liveIndicator = Color(hex: "#FF3B30")
+}
--- 2Gather/Themes/Typography.swift
+++ 2Gather/Themes/Typography.swift
@@ -5,3 +5,30 @@
 //  Created by Muhammad Darrel Prawira on 25/05/26.
 //
 
+import SwiftUI
+
+// MARK: - Typography Scale
+
+extension Font {
+
+    // MARK: Display
+    static let displayXL: Font = .system(size: 64, weight: .black, design: .rounded)
+    static let displayL:  Font = .system(size: 48, weight: .black, design: .rounded)
+    static let displayM:  Font = .system(size: 36, weight: .bold,  design: .rounded)
+
+    // MARK: Heading
+    static let headingL: Font = .system(size: 24, weight: .bold)
+    static let headingM: Font = .system(size: 20, weight: .semibold)
+    static let headingS: Font = .system(size: 17, weight: .semibold)
+
+    // MARK: Body
+    static let bodyL:  Font = .system(size: 17, weight: .regular)
+    static let bodyM:  Font = .system(size: 15, weight: .regular)
+    static let bodyS:  Font = .system(size: 13, weight: .regular)
+    static let bodyXS: Font = .system(size: 11, weight: .regular)
+
+    // MARK: Label
+    static let labelL: Font = .system(size: 15, weight: .medium)
+    static let labelM: Font = .system(size: 13, weight: .medium)
+    static let labelS: Font = .system(size: 11, weight: .medium)
+}
